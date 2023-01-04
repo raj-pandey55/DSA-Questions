d0e1a03daa1dfc6d64abda6950b16d3de0dc299e
Raj Pandey <rajpandey552002@gmail.com>
2023-01-04 21:08:23 +0530

    chore: testing git

---
+++ test.py
@@ -0,0 +1,31 @@
+arr1=[1,3,5,7]
+arr2=[0,2,4,6]
+arr3=[]
+# n1=int(input())
+
+# for i in range(0,n1):
+#   temp=int(input())
+#   arr1.append(temp)
+
+# n2=int(input())
+
+# for i in range(0,n2):
+#   temp=int(input())
+#   arr2.append(temp)
+
+j=0
+k=0
+
+while(1):
+  if(len(arr3)==len(arr1)+len(arr2)):
+    if(arr1[j]>arr2[k]):
+      arr3.append(arr1[j])
+      j+=1
+    else:
+      arr3.append(arr2[k])
+      k+=1
+  break
+
+
+for i in range(0,len(arr1)+len(arr2)):
+  print(arr3[i])
